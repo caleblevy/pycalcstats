c83db4a2ffa0f8c9636d11314437fdea921058f3
geremy condra <debatem1@gmail.com>
2010-10-21 18:16:18 -0700

    Added skeleton to test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+"""
+test.py
+
+Written by Geremy Condra
+Licensed under the Python License
+Released 21 October 2010
+
+This module contains some lightweight tests for the stats module.
+"""
+
+import unittest
+
+class UsabilityTests(unittest.TestCase):
+	pass
+
+class PrecisionTests(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
