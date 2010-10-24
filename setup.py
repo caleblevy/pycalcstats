37c972880bb74718ac1ca48870f1e59038f87d3c
geremy condra <debatem1@gmail.com>
2010-10-23 19:35:41 -0700

    Merged changes between your version of stats.py and mine, added distributions.py, etc.

--- setup.py
+++ setup.py
@@ -1,48 +1,13 @@
+#! /usr/bin/env python3
+
 from distutils.core import setup
 
-from stats import __version__, __author__, __author_email__
-
-setup(
-    name = "stats",
-    py_modules=["stats"],
-    version = __version__,
-    author = __author__,
-    author_email = __author_email__,
-    url = 'http://pypi.python.org/pypi/stats',
-    keywords = ["statistics", "mathematics", "calculator"],
-    description = "Calculator-style statistical functions",
-    long_description = """\
-Statistical functions
----------------------
-
-stats is a pure-Python module providing basic statistics functions
-similar to those found on scientific calculators. It currently includes:
-
-Univariate statistics including:
-* arithmetic, harmonic, geometric and quadratic means
-* median, mode
-* standard deviation and variance (sample and population)
-
-Multivariate statistics including:
-* Pearson's correlation coefficient
-* covariance (sample and population)
-* linear regression
-
-and others.
-
-Requires Python 3.1.
-""",
-    license = 'MIT',  # apologies for the American spelling
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.1",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "License :: OSI Approved :: MIT License",
-        ],
-    )
-
+setup(  name="stats.py",
+	    version="0.1a",
+    	description="Elementary statistics module for Python",
+	    author="Steven D'Aprano",
+	    author_email="steve+python@pearwood.info",
+	    url="http://pypi.python.org/pypi/stats/0.1a",
+	    package_dir={'': 'src'},
+        packages=['stats']
+)
