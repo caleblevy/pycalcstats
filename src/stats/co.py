5cd3d65c2c5f8ed7297e1110b8d25f23f9531c83
Steven D'Aprano <steve+python@pearwood.info>
2011-04-17 14:34:02 +1000

    Much work on co module and tests for same.

--- src/stats/co.py
+++ src/stats/co.py
@@ -4,12 +4,42 @@
 ##  See the file __init__.py for the licence terms for this software.
 
 """
-Coroutine-based statistics functions
-------------------------------------
+The ``stats.co`` module provides eight coroutine based statistics functions:
 
-These functions provide a consumer model for calculating statistics using
-coroutines. For example, to calculate a running sum, you use the send()
-method to pass values into the consumer, and get the running sum back:
+    Function        Description
+    ==============  ==========================================
+    corr            Correlation coefficient of (X, Y) data.
+    ewma            Exponentially weighted moving average.
+    mean            Running arithmetic mean (average).
+    pstdev          Population standard deviation of data.
+    pvariance       Population variance of data.
+    stdev           Sample standard deviation of data.
+    sum             Running sum of data.
+    variance        Sample variance of data (bias-corrected).
+
+The function ``stats.co.sum`` is an alias to ``stats.running_sum``.
+
+The module also includes one public utility function:
+
+    Name                Description
+    ==================  =============================================
+    feed                Convert coroutines into iterators.
+
+
+
+Consumers
+---------
+
+The functions in this module use a consumer model based on coroutines.
+Instead of passing the entire data set to the function as a single argument,
+you pass each data point to the function one at a time, and the result is
+incrementally calculated. This is ideal for calculating running statistics
+or for dealing with data sets that is produced lazily, particularly if you
+have a very large data stream and don't want to, or can't, make multiple
+passes over the data.
+
+For example, to calculate a running sum, you use the send() method to pass
+values into the consumer, and get the running sum back:
 
 >>> import stats.co
 >>> running_sum = stats.co.sum()
@@ -19,26 +49,29 @@ method to pass values into the consumer, and get the running sum back:
 65
 
 Each time you send a value into the consumer, the running total is updated
-and returned. A consumer version of the mean is also provided:
+and returned. Similarly, to calculate a running mean:
 
->>> rmean = stats.co.mean()
->>> rmean.send(1)
-1.0
->>> rmean.send(5)
+>>> running_mean = stats.co.mean()
+>>> running_mean.send(2)
+2.0
+>>> running_mean.send(3)
+2.5
+>>> running_mean.send(4)
 3.0
 
-Consumer versions of variance and stdev functions (both population and
-sample) and Pearson's correlation coefficient are also provided. They may
-be particularly useful when you have a very large data stream and don't
-want to make multiple passes over the data.
 
-To convert a consumer into a producer, use the helper function `feed`. This
-takes two arguments, a consumer such as a coroutine and an iterable data
-source, and returns a generator object that yields the output of sending
-data into the consumer:
 
->>> rsum = stats.co.sum()  # Create a consumer of data.
->>> it = stats.co.feed(rsum, [1, 4, 7])  # Turn it into a producer.
+Producers
+---------
+
+Consumers take their input lazily; producers give up their output lazily.
+To convert a consumer into a producer, use the helper function ``feed``.
+This takes two arguments, a coroutine (the consumer) and an iterable data
+source, and returns a iterator that yields the output of sending data into
+the consumer. For example:
+
+>>> running_sum = stats.co.sum()  # Create a consumer of data.
+>>> it = stats.co.feed(running_sum, [1, 4, 7])  # Turn it into a producer.
 >>> next(it)
 1
 >>> next(it)
@@ -46,25 +79,11 @@ data into the consumer:
 >>> next(it)
 12
 
-The weighted_running_average consumer accepts data points and returns the
-running average of the current data point and the previous average:
-
->>> aver = stats.co.weighted_running_average()
->>> aver.send(3)
-3
->>> aver.send(5)
-4.0
->>> aver.send(2)
-3.0
->>> it = stats.co.feed(aver, [1, 4, 7])
->>> list(it)
-[2.0, 3.0, 5.0]
-
 """
 
 __all__ = [
-    'feed', 'sum', 'mean', 'weighted_running_average',
-    'pvariance', 'variance', 'pstdev', 'stdev', 'corr',
+    'corr', 'ewma', 'feed', 'mean', 'pstdev', 'pvariance', 'stdev',
+    'sum', 'variance',
     ]
 
 
@@ -72,8 +91,10 @@ import collections
 import itertools
 import math
 
+import stats
+
 from builtins import sum as _sum
-from stats.utils import StatsError, coroutine, add_partial
+
 
 
 # === Utilities and helpers ===
@@ -85,7 +106,7 @@ def feed(consumer, iterable):
     feed() returns a generator that yields items from the given coroutine
     and iterator.
 
-    >>> def counter():              # Consumer that counts items sent in.
+    >>> def counter():  # Consumer that counts items sent in.
     ...     c = 0
     ...     _ = (yield None)
     ...     while True:
@@ -106,37 +127,9 @@ def feed(consumer, iterable):
 
 # === Sums and averages ===
 
-@coroutine
-def sum(start=None):
-    """Running sum co-routine.
+from stats import running_sum as sum
 
-    With no arguments, sum() consumes values and returns the running sum:
-
-    >>> rsum = sum()
-    >>> rsum.send(1)
-    1
-    >>> [rsum.send(n) for n in (2, 3, 4)]
-    [3, 6, 10]
-
-    If optional argument start is given and is not None, it is used as the
-    initial value for the running sum:
-
-    >>> rsum = sum(9)
-    >>> [rsum.send(n) for n in (1, 2, 3)]
-    [10, 12, 15]
-
-    """
-    if start is not None:
-        total = [start]
-    else:
-        total = []
-    x = (yield None)
-    while True:
-        add_partial(x, total)
-        x = (yield _sum(total))
-
-
-@coroutine
+@stats.coroutine
 def mean():
     """Running mean co-routine.
 
@@ -148,67 +141,96 @@ def mean():
     >>> [aver.send(n) for n in (2, 3, 4)]
     [1.5, 2.0, 2.5]
 
-    The running average, or cumulative moving average, consumes data
+    The running average, also known as the cumulative moving average,
+    consumes data:
+
         a, b, c, d, ...
 
     and returns the values:
-        a/1, (a+b)/2, (a+b+c)/3, (a+b+c+d)/4, ...
+
+        a, (a+b)/2, (a+b+c)/3, (a+b+c+d)/4, ...
 
     >>> aver = mean()
     >>> [aver.send(n) for n in (40, 30, 50, 46, 39, 44)]
     [40.0, 35.0, 40.0, 41.5, 41.0, 41.5]
 
     """
-    total = []
     n = 0
+    running_sum = sum()
     x = (yield None)
     while True:
-        add_partial(x, total)
+        total = running_sum.send(x)
         n += 1
-        x = (yield _sum(total)/n)
+        x = (yield total/n)
 
 
-@coroutine
-def weighted_running_average():
-    """Weighted running average co-routine.
+@stats.coroutine
+def ewma(alpha=0.5):
+    """Exponentially weighted moving average (EWMA).
 
-    weighted_running_average() consumes values and returns a running average
-    with exponentially decreasing weights. The first value returned is the
-    first data point consumed; after that, each value is the average between
-    the previous average and the most-recent data point:
+    Coroutine returning a moving average with exponentially decreasing
+    weights. The first value returned is the first data point consumed.
 
-    >>> aver = weighted_running_average()
+    Optional parameter ``alpha`` controls the degree by which the weights
+    decrease. It must be a number, and should be between 0 and 1 exclusive.
+    (Values of ``alpha`` outside of this range are not prohibited, but the
+    results may not be physically or statistically meaningful.) Each
+    moving average after the initial point is given by:
+
+        alpha * current data point + (1 - alpha) * previous average
+
+    >>> aver = ewma(0.25)
+    >>> aver.send(3)
+    3
+    >>> aver.send(5)
+    3.5
+    >>> aver.send(2)
+    3.125
+    >>> aver.send(4)
+    3.34375
+
+    By default ``alpha`` is one half, which is equivalent to averaging each
+    value (after the first) with the previous moving average:
+
+    >>> aver = ewma()
     >>> aver.send(5)
     5
     >>> aver.send(1)  # average of 5 and 1
     3.0
     >>> aver.send(2)  # average of 3 and 2
     2.5
+    >>> it = feed(aver, [4.5, 2.5, 4.0])
+    >>> list(it)
+    [3.5, 3.0, 3.5]
 
-    The weighted running average consumes data
-        a, b, c, d, ...
-
-    and returns the values:
-        a, (a+b)/2, ((a+b)/2 + c)/2, (((a+b)/2 + c)/2 + d)/2, ...
-
-    The values returned are weighted means, where the weight on older points
-    decreases exponentially.
-
-    >>> aver = weighted_running_average()
+    >>> aver = ewma()
     >>> [aver.send(n) for n in (40, 30, 50, 46, 39, 44)]
     [40, 35.0, 42.5, 44.25, 41.625, 42.8125]
 
     """
-    ca = (yield None)
-    x = (yield ca)
+    if not stats._is_numeric(alpha):
+        raise stats.StatsError('alpha must be a number')
+    complement_alpha = 1 - alpha
+    moving_average = (yield None)
+    x = (yield moving_average)
     while True:
-        ca = (ca + x)/2
-        x = (yield ca)
+        try:
+            moving_average = alpha*x + complement_alpha*moving_average
+        except TypeError:
+            if not stats._is_numeric(x):
+                raise
+            # Downgrade to floats and try again.
+            x = float(x)
+            moving_average = float(moving_average)
+            alpha = float(alpha)
+            complement_alpha = 1 - alpha
+            continue
+        x = (yield moving_average)
 
 
 # === Measures of spread ===
 
-@coroutine
+@stats.coroutine
 def _welford():
     """Welford's method of calculating the running variance.
 
@@ -227,19 +249,19 @@ def _welford():
     while True:
         delta = x - m
         m += delta/n  # Update the mean.
-        add_partial(delta*(x-m), M2_partials)  # Update the second moment.
+        stats.add_partial(delta*(x-m), M2_partials)  # Update the 2nd moment.
         M2 = _sum(M2_partials)
         assert M2 >= 0.0
         x = (yield (n, M2))
         n += 1
 
 
-@coroutine
+@stats.coroutine
 def pvariance():
     """Running population variance co-routine.
 
-    pvariance() consumes values and returns the population variance of the
-    data points seen so far:
+    ``pvariance`` consumes values and returns the variance with N degrees of
+    freedom of the data points seen so far:
 
     >>> data = [0.25, 0.5, 1.25, 1.25, 1.75, 2.75, 3.5]
     >>> rvar = pvariance()
@@ -254,11 +276,6 @@ def pvariance():
     0.67534722222...
     1.17602040816...
 
-    This may be especially useful when you can only afford a single pass
-    through the data.
-
-    If your data represents a statistical sample rather than the entire
-    population, then you should use variance instead.
     """
     cr = _welford()
     x = (yield None)
@@ -267,13 +284,16 @@ def pvariance():
         n, M2 = cr.send((yield M2/n))
 
 
-@coroutine
+@stats.coroutine
 def variance():
     """Running sample variance co-routine.
 
-    variance() consumes values and returns the sample variance of the
-    data points seen so far. Note that the sample variance is never defined
-    for a single data point, and a float NAN will always be returned.
+    ``variance`` consumes values and returns the variance with N-1 degrees
+    of freedom of the data points seen so far.
+
+        WARNING: The sample variance with N-1 degrees of freedom
+        is not defined for a single data point. The first result
+        given by ``variance`` will always be a NAN.
 
     >>> data = [0.25, 0.5, 1.25, 1.25, 1.75, 2.75, 3.5]
     >>> rvar = variance()
@@ -288,28 +308,23 @@ def variance():
     0.81041666666...
     1.37202380952...
 
-    This may be especially useful when you can only afford a single pass
-    through the data.
-
-    If your data represents the entire population rather than a statistical
-    sample, then you should use pvariance instead.
     """
     cr = _welford()
     x = (yield None)
     n, M2 = cr.send(x)
-    assert n == 1 and M2 == 0.0
+    assert n == 1 and M2 == 0
     x = (yield float('nan'))
     n, M2 = cr.send(x)
     while True:
         n, M2 = cr.send((yield M2/(n-1)))
 
 
-@coroutine
+@stats.coroutine
 def pstdev():
     """Running population standard deviation co-routine.
 
-    pstdev() consumes values and returns the population standard deviation
-    of the data points seen so far:
+    ``pstdev`` consumes values and returns the standard deviation with N
+    degrees of freedom for the data points seen so far:
 
     >>> data = [1.75, 0.25, 1.25, 3.5, 2.75, 1.25, 0.5]
     >>> rsd = pstdev()
@@ -324,11 +339,6 @@ def pstdev():
     1.0647443616
     1.08444474648...
 
-    This may be especially useful when you can only afford a single pass
-    through the data.
-
-    If your data represents a statistical sample rather than the entire
-    population, then you should use stdev instead.
     """
     var = pvariance()
     x = (yield None)
@@ -337,14 +347,16 @@ def pstdev():
         x = var.send((yield math.sqrt(x)))
 
 
-@coroutine
+@stats.coroutine
 def stdev():
     """Running sample standard deviation co-routine.
 
-    stdev() consumes values and returns the sample standard deviation
-    of the data points seen so far. Note that the sample standard deviation
-    is never defined for a single data point, and a float NAN will always
-    be returned.
+    ``stdev`` consumes values and returns the standard deviation with N-1
+    degrees of freedom for the the data points seen so far.
+
+        WARNING: The sample standard deviation with N-1 degrees of
+        freedom is not defined for a single data point. The first
+        result given by ``stdev`` will always be a NAN.
 
     >>> data = [1.75, 0.25, 1.25, 3.5, 2.75, 1.25, 0.5]
     >>> rsd = stdev()
@@ -359,11 +371,6 @@ def stdev():
     1.16636900965...
     1.17133420061...
 
-    This may be especially useful when you can only afford a single pass
-    through the data.
-
-    If your data represents the entire population rather than a statistical
-    sample, then you should use pstdev instead.
     """
     var = variance()
     x = (yield None)
@@ -378,6 +385,8 @@ def stdev():
 # FIX ME
 def _terriberry(data):
     """Terriberry's algorithm for a single pass estimate of skew and kurtosis.
+
+    This is (currently) completely untested and unsupported.
 
     This calculates the second, third and fourth moments
         M2 = sum( (x-m)**2 )
@@ -413,23 +422,25 @@ def _calc_r(sumsqx, sumsqy, sumco):
        return float('nan')
     r = sumco/den
     # -1 <= r <= +1 should hold, but due to rounding errors sometimes the
-    # absolute value of r can exceed 1 by up to 2**-51. We accept this
+    # absolute value of r can exceed 1 by up to 2**-50. We accept this
     # without comment.
     excess = max(abs(r) - 1.0, 0.0)
-    if 0 < excess <= 2**-51:
+    if 0 < excess <= 2**-50:
         r = math.copysign(1, r)
     assert -1.0 <= r <= 1.0, "expected -1.0 <= r <= 1.0 but got r = %r" % r
     return r
 
 
-@coroutine
+@stats.coroutine
 def corr():
-    """Running Pearson's correlation coefficient coroutine.
+    """Running Pearson's correlation coefficient coroutine ``r``.
 
-    corr() consumes (x,y) pairs and returns r, the Pearson's correlation
-    coefficient, of the data points seen so far. Note that r requires at
-    least two pairs of data to be defined, and consequently the first value
-    returned will be a float NAN.
+    ``corr`` consumes (X,Y) pairs and returns ``r``, the sample Pearson's
+    correlation coefficient, of the data points seen so far.
+
+        WARNING: Pearson's correlation coefficient (with N-1 degrees
+        of freedom) is not defined for a single data point. The first
+        result given by ``corr`` will always be a NAN.
 
     >>> xdata = [0, 5, 4, 9, 8, 4, 3]
     >>> ydata = [1, 2, 4, 8, 6, 3, 4]
@@ -445,12 +456,16 @@ def corr():
     0.903737838894
     0.875341049362
 
-    This may be especially useful when you can only afford a single pass
-    through the data.
+    The correlation coefficient ``r`` is a number between -1 and 1 inclusive.
+    It gives a measure of the strength of *linear* relationship between the
+    X and Y coordinates of the data. A correlation of 1 implies a perfect
+    linear relationship between the X and Y values, where increasing X gives
+    increasing Y, and vice versa. A correlation of -1 similarly implies a
+    perfect linear relationship where increasing X gives decreasing Y,
+    and vice versa (a perfect anti-correlation). A correlation of zero
+    implies no linear relationship between the X and Y coordinates.
 
-    r is always between -1 and 1 inclusive. If the estimated variance of
-    either the x or the y data points is zero (e.g. if they are constant),
-    or if their product underflows to zero, a float NAN will be returned.
+    ``r`` is always between -1 and 1 inclusive, unless it is a NAN.
     """
     sumsqx = 0  # sum of the squares of the x values
     sumsqy = 0  # sum of the squares of the y values
